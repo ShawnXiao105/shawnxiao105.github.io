52446e16bb86218bbe97ad00a60334dea53ce623
bigdata <yang.xiao105@gmail.com>
2017-10-17 02:10:29 +0800

    sync map app

--- site-map.js
+++ site-map.js
@@ -6,8 +6,14 @@ require([
   "esri/dijit/Scalebar",
   "esri/dijit/LayerList",
   "esri/dijit/Search",
+  "dojo/i18n!esri/nls/jsapi",
   "dojo/query",
   "esri/arcgis/utils",
+  "esri/layers/FeatureLayer",
+  "esri/dijit/FeatureTable",
+  "esri/dijit/Legend",
+  "dojo/_base/array",
+  "dojo/parser",
 
   // Calcite Maps
   "calcite-maps/calcitemaps-v0.4",
@@ -19,19 +25,22 @@ require([
   "bootstrap/Modal",
 
   "dojo/domReady!"
-], function(Map, Scalebar, LayerList, Search, query, arcgisUtils, CalciteMaps) {
-
+], function(Map, Scalebar, LayerList, Search, esriBundle, query, arcgisUtils, FeatureLayer, FeatureTable, Legend, arrayUtils, parser, CalciteMaps) {
+  //parser.parse();
+  esriBundle.widgets.Search.main.placeholder = "Find site or cell";
+  
   // App 
   app = {
     map: null,
     scalebar: null,
     layerList: null,
     basemap: "topo",
-    center: [114.19149,22.28597], // lon, lat
-    zoom: 11,
+    center: [-96.53, 38.374],
+        zoom: 13,
     initialExtent: null,
     searchWidgetNav: null,
-    searchWidgetPanel: null
+    searchWidgetPanel: null,
+    legend:null,
   }
 
   // Map 
@@ -53,7 +62,47 @@ require([
       scalebarUnit: "dual"
     });
   })
- 
+  
+  // Create the feature layer
+        var myFeatureLayer = new FeatureLayer("https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/california_census_blocks/FeatureServer/0", {
+          mode: FeatureLayer.MODE_ONDEMAND,
+          outFields:  ["NAME","GEOID","MTFCC","ALAND","AWATER"],
+          visible: true,
+          id: "fLayer"
+        });
+
+        myTable = new FeatureTable({
+          featureLayer : myFeatureLayer,
+          showGridMenu: false,
+          hiddenFields: ["FID","C_Seq","Street"]  // field that end-user can show, but is hidden on startup
+        }, "alarmsDiv");
+
+        myTable.startup();
+
+
+        var rivers = new FeatureLayer("https://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Hydrography/Watershed173811/MapServer/1", {
+        mode: FeatureLayer.MODE_ONDEMAND,
+        outFields: ["*"]
+      });
+      var waterbodies = new FeatureLayer("https://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Hydrography/Watershed173811/MapServer/0", {
+        mode: FeatureLayer.MODE_ONDEMAND,
+        outFields: ["*"]
+      });
+
+      //add the legend
+      app.map.on("layers-add-result", function (evt) {
+        var layerInfo = arrayUtils.map(evt.layers, function (layer, index) {
+          return {layer:layer.layer, title:layer.layer.name};
+        });
+        if (layerInfo.length > 0) {
+           app.legend = new Legend({
+            map: app.map,
+            layerInfos: layerInfo
+          }, "legendDiv");
+          app.legend.startup();
+        }
+      });
+app.map.addLayers([waterbodies, rivers]);
   //layerList
   app.layerList = createLayerListWidget("layersDiv");
 
@@ -75,6 +124,15 @@ require([
     }, parentId);
     search.startup();
     return search;
+  }
+
+  function createLegendWidget(parentId) {
+    var legendDijit = new Legend({
+            map: app.map,
+            layerInfos: layerInfo
+          }, "legendDiv");
+    app.legendDijit.startup(); 
+    return legendDijit;
   }
 
   // Basemaps
